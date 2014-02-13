4ba28536ee1eea62f6797796de12ab8871487527
Frank Carnevale <frank.carnevale@gmail.com>
2014-02-13 14:23:20 -0500

    Turn off tabs in Sublime replace all tabs with two spaces

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do 
-	factory :user do
-		name			"Miguel Hartillio"
-		email			"miguel@example.com"
-		password	"foobar"
-		password_confirmation "foobar"
-	end
+  factory :user do
+    name      "Miguel Hartilio"
+    email     "miguel@example.com"
+    password  "foobar"
+    password_confirmation "foobar"
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-	
+  
   subject { page }
 
   describe "Home page" do
@@ -15,20 +15,20 @@ describe "StaticPages" do
   describe "Help page" do
     before { visit help_path }
 
-  	it { should have_content('Help') }
-  	it { should have_title(full_title('Help')) }
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-  	it { should have_content('About Us') }		
-		it { should have_title(full_title('About Us')) }
+    it { should have_content('About Us') }    
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-  	
+    
     it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
